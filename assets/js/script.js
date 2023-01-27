34f8ed7cf63f710cf739b062c5229a9467bd1a50
Al Turing <jongs@Jons-iMac.local>
2023-01-27 10:44:02 +0000

    Added part functionality to save button. Click is working, and saving to localStorage object. Also gets localstorage on page load. Not yet assigning value updates to storage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,72 @@
 // Wrap all code in document ready function so that the JS doesn't run until all elements on the page have loaded
 $(document).ready(function () {
-  const workingHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+  const workingHours = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
   const container = $(".container");
   const currentDay = $("#currentDay");
-  let displayDate = moment().format("dddd, Do of MMMM YYYY, h:mm a");
+  let displayDate = moment().format("dddd, Do of MMMM YYYY");
   currentDay.text(displayDate);
+
+  let plannerStorage = {
+    8: {
+      text: "",
+      type: "Work",
+      saved: "",
+    },
+    9: {
+      text: "",
+      type: "Work",
+      saved: "",
+    },
+    10: {
+      text: "",
+      type: "Work",
+      saved: "",
+    },
+    11: {
+      text: "",
+      type: "Work",
+      saved: "",
+    },
+    12: {
+      text: "",
+      type: "Work",
+      saved: "",
+    },
+    13: {
+      text: "",
+      type: "Work",
+      saved: "",
+    },
+    14: {
+      text: "",
+      type: "Work",
+      saved: "",
+    },
+    15: {
+      text: "",
+      type: "Work",
+      saved: "",
+    },
+    16: {
+      text: "",
+      type: "Work",
+      saved: "",
+    },
+    17: {
+      text: "",
+      type: "Work",
+      saved: "",
+    },
+    18: {
+      text: "",
+      type: "Work",
+      saved: "",
+    },
+  };
 
   // Updates the current time. Note: Run setInterval to handle updating the page every second
   function updateTime() {
-    displayDate = moment().format("dddd, Do of MMMM YYYY, HH:mm");
+    displayDate = moment().format("dddd, Do of MMMM YYYY");
     currentDay.text(displayDate);
 
     // For loop to continually check time for restyling rows
@@ -42,14 +100,32 @@ $(document).ready(function () {
       let buttonAdd = $("<button>").addClass("button saveBtn col-md-1");
       let buttonIconAdd = $("<i>").addClass("fa-regular fa-floppy-disk");
       selectAdd.append(workOpt).append(personalOpt).append(otherOpt);
-      inputAdd
-        .append(hourAdd)
-        .append(textAreaAdd)
-        .append(selectAdd)
-        .append(buttonAdd);
+      inputAdd.append(hourAdd, textAreaAdd, selectAdd, buttonAdd);
       buttonAdd.append(buttonIconAdd);
     }
+    $("#8").children().eq(0).text("Morning");
+    $("#18").children().eq(0).text("Evening");
   }
+
+  function fromStorage() {
+    let plannerObject = JSON.parse(localStorage.getItem("plannerObject"));
+
+    if (plannerObject === null) {
+      plannerObject = plannerStorage;
+    }
+    return plannerObject;
+  }
+
+  function toStorage() {
+    localStorage.setItem("plannerObject", JSON.stringify(plannerStorage));
+  }
+
+  $(document).on("click", ".saveBtn", function (event) {
+    let parentID = $(event.target).parent().attr("id");
+    toStorage();
+  });
+
+  let plannerObject = fromStorage();
   setInterval(updateTime, 1000);
   createDiary();
 });
